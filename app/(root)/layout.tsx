36490ee4a5ff3b379550a61752be2157c62a2387
Yab1 <yabilisanu@gmail.com>
2023-12-30 16:54:18 +0300

    Refactor root layout to include top, left, and right sidebars
    Reorganize the root layout to incorporate distinct sections for top,
    left, and right sidebars, enhancing the overall structure and
    functionality. This change improves the layout's modularity and
    facilitates better component management. No associated issue.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,12 @@ import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import "@/app/globals.css";
+import {
+  Bottombar,
+  LeftSidebar,
+  RightSidebar,
+  Topbar,
+} from "@/components/shared";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +25,15 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${inter.className} bg-black-1`}>
-          <main>{children}</main>
+          <Topbar />
+          <main>
+            <LeftSidebar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottombar />
         </body>
       </html>
     </ClerkProvider>
